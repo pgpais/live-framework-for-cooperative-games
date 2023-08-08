c532fda4b79b115837bd0bdf146d4e548637327f
Pedro Pais <pgpaisdev@gmail.com>
2023-08-08 12:53:31 +0100

    init

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="justify-between flex bg-black p-5">
+	<div>Put logo here pls</div>
+	<div>
+		<h1 class="text-6xl font-bold">Hello</h1>
+	</div>
+	<div>
+		<p>put my stuff at the end</p>
+		<LightSwitch />
+		<button class="btn variant-filled" type="button">I do nothing</button>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Header from '$lib/components/Header.svelte';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
+</script>
+
+<svelte:head
+	>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head
+>
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<Header />
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
