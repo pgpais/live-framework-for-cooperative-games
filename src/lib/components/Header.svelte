35b1c998e028d12654e5af23a8b123227c89f016
Pedro Pais <pgpaisdev@gmail.com>
2023-08-18 12:42:45 +0100

    Highlighting of navbar when in page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,23 @@
 <script>
+	import { page } from '$app/stores';
 	import { LightSwitch, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="bg-surface-100-800-token flex justify-between p-5">
 	<div>logo</div>
 	<TabGroup>
-		<TabAnchor href="/frameworks">Framework</TabAnchor>
-		<TabAnchor href="/reports/new">New Report</TabAnchor>
-		<TabAnchor href="/frameworks/edit">New Framework</TabAnchor>
-		<TabAnchor href="/reports">Game Analysis</TabAnchor>
+		<TabAnchor href="/frameworks" selected={$page.url.pathname === '/frameworks'}>
+			Framework
+		</TabAnchor>
+		<TabAnchor href="/reports/new" selected={$page.url.pathname === '/reports/new'}>
+			New Report
+		</TabAnchor>
+		<TabAnchor href="/frameworks/edit" selected={$page.url.pathname === '/frameworks/edit'}>
+			New Framework
+		</TabAnchor>
+		<TabAnchor href="/reports" selected={$page.url.pathname === '/reports'}>
+			Game Analysis
+		</TabAnchor>
 		<!-- <TabAnchor href="/dimensions">Dimensions</TabAnchor> -->
 	</TabGroup>
 	<div>
