6063749d6620764909c052f2b8abe05f37d9a553
Pedro Pais <pgpaisdev@gmail.com>
2023-08-31 15:15:00 +0100

    trying to get lucia auth working

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,40 @@
-import { DATABASE_URL } from '$env/static/private';
+import { DATABASE_URL, DEV } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
+import { lucia } from 'lucia';
+import { postgres as postgresAdapter } from '@lucia-auth/adapter-postgresql';
+import { sveltekit } from 'lucia/middleware';
 
-const client = postgres(DATABASE_URL);
+import { github } from '@lucia-auth/oauth/providers';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+
+export const client = postgres(DATABASE_URL);
+
 const db = drizzle(client, { schema });
 
+const adapter = postgresAdapter(client, {
+	user: 'auth_user',
+	session: 'user_session',
+	key: 'user_key'
+});
+
+const auth = lucia({
+	env: DEV ? 'DEV' : 'PROD',
+	adapter: adapter,
+	middleware: sveltekit(),
+
+	getUserAttributes: (data) => {
+		return {
+			githubUsername: data.github_username
+		};
+	}
+});
+
+export const githubAuth = github(auth, {
+	clientId: GITHUB_CLIENT_ID,
+	clientSecret: GITHUB_CLIENT_SECRET
+});
+
 export default db;
+export type Auth = typeof auth;
--- src/lib/db/schema/user.ts
+++ src/lib/db/schema/user.ts
@@ -1,13 +1,46 @@
 import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
-import { pgTable, text, serial, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, text, serial, timestamp, varchar, bigint } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
-	id: serial('id').primaryKey(),
+export const users = pgTable('auth_user', {
+	id: varchar('id', {
+		length: 15 // change this when using custom user ids
+	}).primaryKey(),
 	fullName: text('full_name').notNull(),
 	email: text('email').unique().notNull(),
 	password: text('password').notNull(),
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at').defaultNow().notNull()
+});
+
+export const sessions = pgTable('user_session', {
+	id: varchar('id', {
+		length: 128
+	}).primaryKey(),
+	userId: varchar('user_id', {
+		length: 15
+	})
+		.notNull()
+		.references(() => users.id),
+	activeExpires: bigint('active_expires', {
+		mode: 'number'
+	}).notNull(),
+	idleExpires: bigint('idle_expires', {
+		mode: 'number'
+	}).notNull()
+});
+
+export const keys = pgTable('user_key', {
+	id: varchar('id', {
+		length: 255
+	}).primaryKey(),
+	userId: varchar('user_id', {
+		length: 15
+	})
+		.notNull()
+		.references(() => users.id),
+	hashedPassword: varchar('hashed_password', {
+		length: 255
+	})
 });
 
 export type User = InferSelectModel<typeof users>; // return type when queried
