4c2fdc956cced1f9c2a54f09206402401bc55b65
Pedro Pais <pgpaisdev@gmail.com>
2023-09-06 10:56:27 +0100

    renamed url param

--- src/routes/frameworks/[[id=integer]]/+page.server.ts
+++ src/routes/frameworks/[[id=integer]]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import { GetFullFrameworkByAuthor, GetFullFrameworkById } from '$lib/utils/frameworkFetchers';
 
 export const load = (async ({ params }) => {
-	const authorId: number = params.author ? +params.author : 1;
+	const authorId: number = params.id ? +params.id : 1;
 
 	const framework = await GetFullFrameworkById(authorId);
 	return { framework };
