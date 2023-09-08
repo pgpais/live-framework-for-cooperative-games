01629ea04a02e72e20d2625b9d6844db228b5191
Pedro Pais <pgpaisdev@gmail.com>
2023-09-08 15:11:33 +0100

    improved look of website

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,30 +3,59 @@
 	import { page } from '$app/stores';
 	import { LightSwitch, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
 	import type { Session, User } from 'lucia';
+	import logo from '$lib/assets/framework-logo-text.png';
+	import { string } from 'zod';
 
 	export let session: Session | null;
 </script>
 
-<div class="bg-surface-100-800-token flex h-full justify-between p-5">
-	<div>logo</div>
-	<TabGroup>
-		<TabAnchor href="/frameworks" selected={$page.url.pathname === '/frameworks'}>
-			Consult Framework
-		</TabAnchor>
-		<TabAnchor href="/frameworks/search" selected={$page.url.pathname === '/frameworks/search'}>
-			Community Frameworks
-		</TabAnchor>
-		<TabAnchor href="/reports/new" selected={$page.url.pathname === '/reports/new'}>
-			New Report
-		</TabAnchor>
-		<TabAnchor href="/frameworks/new" selected={$page.url.pathname === '/frameworks/edit'}>
-			New Framework
-		</TabAnchor>
-		<TabAnchor href="/reports/search" selected={$page.url.pathname === '/reports/search'}>
-			Game Analysis
-		</TabAnchor>
-		<!-- <TabAnchor href="/dimensions">Dimensions</TabAnchor> -->
-	</TabGroup>
+<div class="bg-surface-800-100-token flex h-full justify-between p-5">
+	<img src={logo} class="h-full" alt="Website logo" />
+	<div class="flex flex-grow flex-col-reverse flex-wrap">
+		<TabGroup
+			hover="hover:variant-filled-secondary"
+			active="variant-soft-surface"
+			justify="justify-evenly"
+			regionList="flex-wrap"
+		>
+			<TabAnchor
+				href="/frameworks"
+				selected={$page.url.pathname.endsWith('/frameworks')}
+				class="text-surface-100-800-token"
+			>
+				Consult Framework
+			</TabAnchor>
+			<TabAnchor
+				href="/frameworks/search"
+				selected={$page.url.pathname.includes('/frameworks/search')}
+				class="text-surface-100-800-token"
+			>
+				Community Frameworks
+			</TabAnchor>
+			<TabAnchor
+				href="/reports/new"
+				selected={$page.url.pathname.includes('/reports/new')}
+				class="text-surface-100-800-token"
+			>
+				New Report
+			</TabAnchor>
+			<TabAnchor
+				href="/frameworks/new"
+				selected={$page.url.pathname.includes('/frameworks/edit')}
+				class="text-surface-100-800-token"
+			>
+				New Framework
+			</TabAnchor>
+			<TabAnchor
+				href="/reports/search"
+				selected={$page.url.pathname.includes('/reports/search')}
+				class="text-surface-100-800-token"
+			>
+				Game Analysis
+			</TabAnchor>
+			<!-- <TabAnchor href="/dimensions">Dimensions</TabAnchor> -->
+		</TabGroup>
+	</div>
 	<div class="flex flex-col">
 		<LightSwitch />
 		{#if session}
