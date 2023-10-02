2e835a15d7a7c7bca548eab00fc8954ac107cb59
Pedro Pais <pgpaisdev@gmail.com>
2023-10-02 15:17:17 +0100

    fixing bugs

--- src/lib/db/migrate.ts
+++ src/lib/db/migrate.ts
@@ -1,4 +1,12 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import db from '$lib/db';
+import * as schema from './schema';
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js/driver';
+
+const dbUrl = process.env.DATABASE_URL_DEV ?? '';
+
+const client = postgres(dbUrl);
+
+const db = drizzle(client, { schema });
 
 await migrate(db, { migrationsFolder: 'drizzle' });
