ee4dbe5223de84c208ea19a0dfb6f5cfd57d06ad
Pedro Pais <pgpaisdev@gmail.com>
2023-10-02 15:22:59 +0100

    fixed import?

--- src/lib/db/migrate.ts
+++ src/lib/db/migrate.ts
@@ -1,7 +1,7 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import * as schema from './schema';
 import postgres from 'postgres';
-import { drizzle } from 'drizzle-orm/postgres-js/driver';
+import { drizzle } from 'drizzle-orm/postgres-js';
 
 const dbUrl = process.env.DATABASE_URL_DEV ?? '';
 
