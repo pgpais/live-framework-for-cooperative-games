124763881e556849a404be166a237bf5a22b8fd6
Pedro Pais <pgpaisdev@gmail.com>
2024-05-23 19:12:50 +0100

    Refactored game reports list into GameCardViews

--- src/lib/components/GameViews/GameCardView.svelte
+++ src/lib/components/GameViews/GameCardView.svelte
@@ -1,40 +1,27 @@
 <script lang="ts">
 	import type { Game, Genre, NewGame } from '$lib/db/schema';
 
-	export let game: NewGame & { genres?: Genre[] };
+	export let game: NewGame & { reportsCount: unknown };
+	export let onClick;
 </script>
 
-<div class="card variant-ghost-tertiary flex p-5">
-	<div class="card-header flex w-1/3 flex-col gap-2">
-		<h3 class="h3">{game.name}</h3>
+<button class="card variant-filled-tertiary card-hover flex flex-col content-around w-60 justify-between my-2" on:click={onClick}>
+	<header class="card-header">
+		<h2 class="card-title"><b>{game.name}</b></h2>
+	</header>
+
+	<section class="flex flex-col gap-2 pl-8 pr-8 place-self-center mt-4 mb-4 justify-between">
+		<p>{game.reportsCount ? game.reportsCount : 0} report{game.reportsCount != 1 ? 's' : ''}</p>
 		{#if game.imgUrl}
-			<img src={game.imgUrl} alt={game.name} class="h-64 w-full object-contain" />
+			<img
+				class="w-full rounded-lg object-scale-down"
+				src={game.imgUrl}
+				alt={'cover of ' + game.name}
+			/>
 		{:else}
 			<div class="variant-outline-warning flex h-64 w-full items-center justify-center">
 				<p class="p">No image available</p>
 			</div>
 		{/if}
-	</div>
-	<div class="flex w-2/3 flex-col gap-4">
-		{#if game.releaseDate}
-			<p class="p">
-				<b>Release Date: </b>{Intl.DateTimeFormat('en-US', {
-					year: 'numeric',
-					month: 'long',
-					day: 'numeric'
-				}).format(new Date(0).setUTCSeconds(+game.releaseDate))}
-			</p>
-		{/if}
-		{#if game.genres}
-			<p class="p">
-				<b>Genres: </b>
-				{#each game.genres as genre}
-					<span>{genre.name}&nbsp;</span>
-				{/each}
-			</p>
-		{/if}
-		{#if game.description}
-			<p class="p"><b>Description: </b>{game.description}</p>
-		{/if}
-	</div>
-</div>
+	</section>
+</button>
--- src/routes/reports/search/+page.svelte
+++ src/routes/reports/search/+page.svelte
@@ -6,6 +6,7 @@
 	import type { PageData } from './$types';
 	import GameDetail from '$lib/components/DetailElements/GameDetail.svelte';
 	import type { Game } from '$lib/db/schema';
+	import GameCardView from '$lib/components/GameViews/GameCardView.svelte';
 
 	export let data: PageData;
 	$: ({ games } = data);
@@ -73,26 +74,7 @@
 		</p>
 		<div class="m-5 grid grid-cols-4">
 			{#each games as game}
-				<button class="card variant-filled-tertiary card-hover flex flex-col content-around w-60 justify-between my-2" on:click={() => (selectedGame = game)}>
-					<header class="card-header">
-						<h2 class="card-title"><b>{game.name}</b></h2>
-					</header>
-
-					<section class="flex flex-col gap-2 pl-8 pr-8 place-self-center mt-4 mb-4 justify-between">
-						<p>{game.reportsCount ? game.reportsCount : 0} report{game.reportsCount != 1 ? 's' : ''}</p>
-						{#if game.imgUrl}
-							<img
-								class="w-full rounded-lg object-scale-down"
-								src={game.imgUrl}
-								alt={'cover of ' + game.name}
-							/>
-						{:else}
-							<div class="variant-outline-warning flex h-64 w-full items-center justify-center">
-								<p class="p">No image available</p>
-							</div>
-						{/if}
-					</section>
-				</button>
+				<GameCardView {game} onClick={() => (selectedGame = game)}/>
 			{/each}
 		</div>
 	</div>
