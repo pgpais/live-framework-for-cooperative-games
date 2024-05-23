9c51ffb6e3c85c3b9ad142140b584ba03fee1ce5
Pedro Pais <pgpaisdev@gmail.com>
2024-05-23 12:25:10 +0100

    added categories to drawer

--- src/lib/components/DetailElements/CategoryDetailButton.svelte
+++ src/lib/components/DetailElements/CategoryDetailButton.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import type { Category, FullCategory } from '$lib/db/schema';
 	import { detailInfoStore, type DetailInfoData } from '$lib/stores/detailView';
+	import { getDrawerStore, type DrawerSettings } from "@skeletonlabs/skeleton";
 
+	const drawerStore = getDrawerStore();
 	export let category: DetailInfoData;
 
-	const changeDetailView = () => {
+	const changeDetailView = (isDrawer: Boolean) => {
 		console.log('updateing detail store');
 
 		detailInfoStore.set({
@@ -13,9 +15,18 @@
 		});
 
 		console.log($detailInfoStore);
+
+		if(isDrawer){
+			const settings: DrawerSettings = { id: 'category-detail' };
+			drawerStore.open(settings);
+		}
 	};
 </script>
 
-<button type="button" on:click={changeDetailView} class="text-start rounded-xl p-1 hover:bg-primary-500/20">
+<button type="button" on:click={() => changeDetailView(false)} class="hidden xl:block text-start rounded-xl p-1 hover:bg-primary-500/20">
+	<b>{category.title}</b>
+</button>
+
+<button type="button" on:click={() => changeDetailView(true)} class="block xl:hidden text-start rounded-xl p-1 hover:bg-primary-500/20">
 	<b>{category.title}</b>
 </button>
