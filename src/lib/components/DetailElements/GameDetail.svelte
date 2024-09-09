1f8014cba4b485333c1831d54c38cdb85c55acf3
Pedro Pais <pgpaisdev@gmail.com>
2024-09-09 15:40:05 +0100

    added drawer for game analysis page

--- src/lib/components/DetailElements/GameDetail.svelte
+++ src/lib/components/DetailElements/GameDetail.svelte
@@ -1,33 +1,46 @@
 <script lang="ts">
 	import type { DimensionExample, Game, Report, User, Dimension } from '$lib/db/schema';
-	import { ArrowBigLeft, ChevronRight } from 'lucide-svelte';
+	import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { error } from '@sveltejs/kit';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
 	console.log('GameDetail.svelte');
 
 	export let game: Game;
-	export let reports: Promise<
+	export let isDrawer: boolean = false;
+
+	const drawerStore = getDrawerStore();
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+
+	let reportsFetch: Promise<
 		(Report & {
 			author: User;
 			dimensionExamples: (DimensionExample & { dimension: Dimension })[];
 		})[]
 	>;
 
-	const reportsFetch: Promise<
-		(Report & {
-			author: User;
-			dimensionExamples: (DimensionExample & { dimension: Dimension })[];
-		})[]
-	> = fetch('/api/reports?game=' + game.id).then((res) => res.json());
+	$: {
+		reportsFetch = fetch('/api/reports?game=' + game.id).then((res) => res.json());
+	}
 </script>
 
 <div class="flex flex-col space-y-4 p-4">
+	<div class="flex items-center">
+		{#if isDrawer}
+			<button on:click={drawerClose} class="p-4">
+				<ChevronLeft />
+			</button>
+		{/if}
+	</div>
 	<img class="h-32 self-center" src={game.imgUrl} alt={'cover of ' + game.name} />
 	<h2 class="h2 text-center text-2xl font-bold">{game.name}</h2>
 	<section><p>{game.description}</p></section>
 	<section>
 		<h3 class="h3">Reports</h3>
 		<hr class="pb-2" />
-		{#await reports}
+		{#await reportsFetch}
 			<div class="grid gap-2">
 				<div class="placeholder animate-pulse" />
 				<div class="placeholder animate-pulse" />
