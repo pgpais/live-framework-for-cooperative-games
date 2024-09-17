8014501f298391beff4d52c934979f8e87c49ada
Pedro Pais <pgpaisdev@gmail.com>
2024-09-17 15:47:56 +0100

    Started working on about page. basic skeleton is done

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,65 @@
-<p>About us</p>
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="m-8 flex w-full flex-col items-center gap-10">
+	<h1 class="h1 text-center">
+		Welcome to the <span class="text-secondary-500-400-token">
+			Living Framework for Understanding Cooperative Games
+		</span>
+	</h1>
+	<div class="flex w-1/2 flex-col items-center gap-5">
+		<div class="card p-5">
+			<p>
+				The Living Framework for Understanding Cooperative Games (LFCG) is derived from a multi-step
+				systematic analysis of 129 cooperative games with contributions of eleven researchers, and
+				was <a
+					href="https://dl.acm.org/doi/pdf/10.1145/3613904.3641953"
+					target="_blank"
+					class="anchor"
+				>
+					published as a scientific article in CHI 2024
+				</a>.
+			</p>
+			<p>
+				LFCG can be used as a tool for analyses and ideation, as well as a shared language for
+				describing a game’s design.
+			</p>
+			<p>It is published as this web application to facilitate use and appropriation.</p>
+		</div>
+		<div>
+			<div class="variant-filled btn">Framework</div>
+			<div class="variant-filled btn">Reports</div>
+		</div>
+		<div class="m-20 flex flex-col items-center gap-5">
+			<h2 class="h2">Authors</h2>
+			<div class="flex justify-between gap-10">
+				<Avatar
+					initials="PP"
+					src="https://techandpeople.github.io/images/people/pgp.jpg"
+					width="w-32"
+				/>
+				<Avatar
+					initials="PP"
+					src="https://techandpeople.github.io/images/people/pgp.jpg"
+					width="w-32"
+				/>
+				<Avatar
+					initials="PP"
+					src="https://techandpeople.github.io/images/people/pgp.jpg"
+					width="w-32"
+				/>
+				<Avatar
+					initials="PP"
+					src="https://techandpeople.github.io/images/people/pgp.jpg"
+					width="w-32"
+				/>
+				<Avatar
+					initials="PP"
+					src="https://techandpeople.github.io/images/people/pgp.jpg"
+					width="w-32"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
