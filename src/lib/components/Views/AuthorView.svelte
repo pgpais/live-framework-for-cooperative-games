f102c42bdca7076867ebbca887eaa780fd19be68
Pedro Pais <pgpaisdev@gmail.com>
2024-09-18 11:53:24 +0100

    Created about page and made it home page (#47)
    * Started working on about page. basic skeleton is done
    
    * Created about page with text and placeholder author views
    
    * changed root page
    
    * fixed text

---
+++ src/lib/components/Views/AuthorView.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	export let src: string;
+	export let name: string;
+	export let width: string;
+</script>
+
+<div class="flex flex-col items-center gap-3">
+	<Avatar initials="PP" {src} {width} />
+	<p>{name}</p>
+</div>
